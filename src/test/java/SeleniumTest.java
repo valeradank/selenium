e9c79090dec7642ea93db259661e973d2dd2eea0
valo <valorapaulinio@gmail.com>
2023-02-13 18:31:46 +0300

    change build, add git ignore

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,53 @@
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumTest {
+
+
+    static WebDriver driver;
+
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+
+
+    }
+
+    @Test
+    void test() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("input[type='text']")).sendKeys("валерий данковцев");
+        driver.findElement(By.cssSelector("input[type='tel']")).sendKeys("+79295964548");
+        driver.findElement(By.tagName("label")).click();
+        driver.findElement(By.className("button__text")).click();
+        String actual = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String expected = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
+
+
+
